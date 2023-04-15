0fbe57631388169ed1d6332c4caa3b46224d9290
Alston Rodriguez <alston1399@gmail.com>
2023-04-14 23:31:57 -0700

    asfg

--- GraphingUtility.js
+++ GraphingUtility.js
@@ -1,3 +1,61 @@
+const PI = Math.PI
+const HALF_PI = PI/2
+const TWO_PI = PI*2
+
+function floor(x) {
+  if (x instanceof Array) {
+    let res = []
+    for (let y of x) res.push(floor(y))
+    return res
+  }
+  return Math.floor(x)
+}
+
+function rand(a, b, c) {
+  if (c == undefined) {
+    if (b == undefined) {
+      if (a == undefined) return Math.random()
+      return rand()*a
+    }
+    return rand(b-a) + a
+  }
+  let res = []
+  for (let i=0; i<c; i++) res.push(rand(a, b))
+  return res
+}
+
+function randInt(a, b, c) {
+  if (c == undefined) {
+    if (b == undefined) return floor(rand(a+1))
+    return randInt(b-a) + a
+  }
+  let res = []
+  for (let i=0; i<c; i++) res.push(randInt(a, b))
+  return res
+}
+
+function map(x,a,b,c,d) {
+  if (x instanceof Array) {
+    let res = []
+    for (let y of x) res.push(map(y,a,b,c,d))
+    return res
+  }
+  return (x-a)/(b-a)*(d-c)+c
+}
+
+function lerp(t, a, b) {
+  return map(t, 0, 1, a, b)
+}
+
+function radToDeg(x) {
+  return x*180/PI
+}
+
+function degToRad(x) {
+  return x*PI/180
+}
+
+
 class AngleMode {
   static Radians = new AngleMode('radians')
   static Degrees = new AngleMode('degrees')
@@ -95,7 +153,7 @@ class Layer {
   }
 }
 
-class GraphingUtil {
+class GraphingUtility {
   constructor(canvas, ...others) {
     this.canvas = canvas
     this.ctx = this.canvas.getContext('2d')
@@ -253,5 +311,5 @@ class GraphingUtil {
 //     grid lines and axis numbers
 
 let canvas = document.createElement('canvas')
-let grapher = new GraphingUtil(canvas, 500, 500)
+let grapher = new GraphingUtility(canvas, 500, 500)
 grapher.appendTo(document.body)
