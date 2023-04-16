fea769b823c83f6acb966ca9f45297c473def925
Alston Rodriguez <alston1399@gmail.com>
2023-04-15 19:48:10 -0700

    Moved reference file into graph file

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
 let canvas = document.createElement('canvas')
-let grapher = new Graph(canvas, [-5,5], [-5,5], 500)
+let grapher = new Graph(canvas, [-10,10], [-10,10], 500)
 grapher.appendTo(document.body)
 grapher.clear()
