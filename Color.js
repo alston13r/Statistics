9e1b9aea349d456053c3112cd3a80422725f9faf
Alston Rodriguez <alston1399@gmail.com>
2023-04-28 15:44:27 -0700

    Removed internal percentage display, added from hex

--- Color.js
+++ Color.js
@@ -1,4 +1,12 @@
 class RGB {
+  static fromHex(s) {
+    if (s.length != 6) s = s.substr(1)
+    s = '0x'+s
+    let r = s>>16&255
+    let g = s>>8&255
+    let b = s&255
+    return new RGB(r, g, b)
+  }
   constructor(r, g, b) {
     this.r = r
     this.g = g
@@ -42,7 +50,7 @@ class RGB {
     let H = this.getHue(rp, gp, bp, delta)
     let L = (max+min)/2
     let S = delta!=0 ? delta/(1-Math.abs(2*L-1)) : 0
-    return new HSL(H>0?Math.round(H):Math.round(360+H), Math.round(S*100), Math.round(L*100))
+    return new HSL(H>0?H:360+H, S, L)
   }
   toHSV() {
     let rp, gp, bp
@@ -52,7 +60,7 @@ class RGB {
 
     let H = this.getHue(rp, gp, bp, delta)
     let S = max!=0?delta/max:0
-    return new HSV(H>0?Math.round(H):Math.round(360+H), Math.round(S*100), Math.round(max*100))
+    return new HSV(H>0?H:360+H, S, max)
   }
   toCMYK() {
     let rp, gp, bp
@@ -61,7 +69,7 @@ class RGB {
     let C = (1-rp-K)/(1-K)
     let M = (1-gp-K)/(1-K)
     let Y = (1-bp-K)/(1-K)
-    return new CMYK(Math.round(C*100), Math.round(M*100), Math.round(Y*100), Math.round(K*100))
+    return new CMYK(C, M, Y, K)
   }
   toHex() {
     return '#' + this.r.toString(16) + this.g.toString(16) + this.b.toString(16)
@@ -83,9 +91,9 @@ class HSL {
     yield this.l
   }
   toRGB() {
-    let C = (1-Math.abs(2*(this.l)/100-1))*this.s/100
+    let C = (1-Math.abs(2*this.l-1))*this.s
     let X = C*(1-Math.abs(this.h/60%2-1))
-    let m = this.l/100-C/2
+    let m = this.l-C/2
     
     X = Math.round((X+m)*255)
     C = Math.round((C+m)*255)
@@ -107,7 +115,7 @@ class HSL {
     return this.toRGB().toHex()
   }
   toString() {
-    return `hsl(${this.h}, ${this.s}%, ${this.l}%)`
+    return `hsl(${round(this.h,0)}, ${round(this.s*100,1)}%, ${round(this.l*100,1)}%)`
   }
 }
 
@@ -123,9 +131,9 @@ class HSV {
     yield this.v
   }
   toRGB() {
-    let C = this.v*this.s/10000
+    let C = this.v*this.s
     let X = C*(1-Math.abs(this.h/60%2-1))
-    let m = this.v/100 - C
+    let m = this.v - C
 
     X = Math.round((X+m)*255)
     C = Math.round((C+m)*255)
@@ -147,7 +155,7 @@ class HSV {
     return this.toRGB().toHex()
   }
   toString() {
-    return `hsv(${this.h}, ${this.s}%, ${this.v}%)`
+    return `hsv(${round(this.h,0)}, ${round(this.s*100,1)}%, ${round(this.v*100,1)}%)`
   }
 }
 
@@ -159,9 +167,9 @@ class CMYK {
     this.k = k
   }
   toRGB() {
-    let R = 255*(1-this.c/100)*(1-this.k/100)
-    let G = 255*(1-this.m/100)*(1-this.k/100)
-    let B = 255*(1-this.y/100)*(1-this.k/100)
+    let R = 255*(1-this.c)*(1-this.k)
+    let G = 255*(1-this.m)*(1-this.k)
+    let B = 255*(1-this.y)*(1-this.k)
     return new RGB(R, G, B)
   }
   toHSL() {
@@ -174,6 +182,6 @@ class CMYK {
     return this.toRGB().toHex()
   }
   toString() {
-    return `cmyk(${this.c}%, ${this.m}%, ${this.y}%, ${this.k}%)`
+    return `cmyk(${round(this.c*100,1)}%, ${round(this.m*100,1)}%, ${round(this.y*100,1)}%, ${round(this.k*100,1)}%)`
   }
 }
