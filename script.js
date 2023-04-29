449356a74f615268716a6ecbc7100255c7808d07
Alston Rodriguez <alston1399@gmail.com>
2023-04-29 00:47:34 -0700

    normal curve testing

--- script.js
+++ script.js
@@ -3,7 +3,4 @@ let grapher = new Graph(canvas, [-5,5], [-1,1], 500)
 grapher.appendTo(document.body)
 grapher.clear()
 grapher.drawAxes()
-let mu = 0
-let stdev = 1
-let fx = x => 1/(stdev*ROOT_TWO_PI)*exp(-0.5*((x-mu)/stdev)**2)
-grapher.graphFunction(fx)
+grapher.graphNormalCurve(0, 1, '#0000ff')
