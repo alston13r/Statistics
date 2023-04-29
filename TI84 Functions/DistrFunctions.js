746b77065640a1d06d90c672a5cfb32ff434cbb9
Alston Rodriguez <alston1399@gmail.com>
2023-04-28 23:40:57 -0700

    removed reference files

--- TI84 Functions/DistrFunctions.js
+++ TI84 Functions/DistrFunctions.js
@@ -15,3 +15,33 @@ function chi2(observed, expected) {
 	}
 	return s
 }
+
+function NormalPDF(x,mu,stdev) {
+	return 1/(stdev*Math.sqrt(2*Math.PI))*Math.exp(-0.5*ZScore(x,mu,stdev)**2)
+}
+
+function NormalCDF(a,b,c,d) {
+	if (d == undefined) return 0.5*(1+math.erf((a-b)/(c*Math.sqrt(2))))
+	else return NormalCDF(b,c,d)-NormalCDF(a,c,d)
+}
+
+function BinomPDF(n,p,k) {
+	let coeff = BinomCoefficient(n,k)
+	return coeff*p**k*(1-p)**(n-k)
+}
+
+function BinomCDF(n,p,k) {
+	let sum = 0
+	for (let i=k;i>=0;i--) {
+		sum += BinomPDF(i,n,p)
+	}
+	return sum
+}
+
+function GeometricPDF(p,k) {
+	return p*(1-p)**(k-1)
+}
+
+function GeometricCDF(p,k) {
+	return 1-(1-p)**k
+}
---
+++ TI84 Functions/MiscFunctions.js
@@ -0,0 +1,35 @@
+function ZScore(x,mu,stdev) {
+	return (x-mu)/stdev
+}
+
+
+function BinomCoefficient(n,k) {
+	if (k > n || k < 0) return 0
+	return PascalTri.getRow(n)[k]
+}
+
+
+const PascalTri = {
+	get len() {
+		return this.rows.length
+	},
+	rows: [[1],[1,1],[1,2,1],[1,3,3,1],[1,4,6,4,1],[1,5,10,10,5,1]],
+	getRow(i) {
+		if (i > this.rows.length-1) this.addRows(i-this.rows.length+1)
+		return this.rows[i]
+	},
+	addRows(n) {
+		for (let i=0;i<n;i++) {
+			this.addRow()
+		}
+	},
+	addRow() {
+		let lastRow = this.rows[this.rows.length-1]
+		let newRow = [1]
+		for (let i=0;i<lastRow.length-1;i++) {
+			newRow.push(lastRow[i]+lastRow[i+1])
+		}
+		newRow.push(1)
+		this.rows.push([...newRow])
+	}
+}
