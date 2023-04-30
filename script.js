1829479538b4971ca68232a4a0bc0e159d176592
Alston Rodriguez <alston1399@gmail.com>
2023-04-29 21:45:16 -0700

    changed canvas size

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 let canvas = document.createElement('canvas')
-let grapher = new Graph(canvas, [-10,10], [-10,10], 900)
+let grapher = new Graph(canvas, [-10,10], [-10,10], 500)
 grapher.appendTo(document.body)
 grapher.clear()
 grapher.drawAxes()
