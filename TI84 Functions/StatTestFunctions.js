f257f496dc268c2c1d7d4349d921f3fe33011d86
Alston Rodriguez <alston1399@gmail.com>
2023-05-02 21:51:07 -0700

    added standard error functions and begun to pop

--- TI84 Functions/StatTestFunctions.js
+++ TI84 Functions/StatTestFunctions.js
@@ -1,3 +1,24 @@
+function StandErrOneProp(p̂, n) {
+  return sqrt(p̂*(1-p̂)/n)
+}
+
+function StandErrTwoProp(p̂1, n1, p̂2, n2) {
+  return sqrt(p̂1*(1-p̂1)/n1+p̂2*(1-p̂2)/n2)
+}
+
+function StandErrOneMean(s, n) {
+  return s/sqrt(n)
+}
+
+function StandErrTwoMean(s1, n1, s2, n2) {
+  return sqrt(s1**2/n1+s2**2/n2)
+}
+
+function StandErrLine(xl, yl) {
+  if (xl.length != yl.length) return
+  return yl.variability(2) / (xl.standardDeviation(true)*sqrt(xl.length-1))
+}
+
 class InputType {
   static Data = new InputType('data')
   static Stats = new InputType('stats')
@@ -15,220 +36,185 @@ class EqualityChoice {
   }
 }
 
-function ZTest(inputType, μ0, ...rest) {
-  if (inputType == InputType.Data) {
-    let list, equalityChoice
-    // a = list
-    // b = equalityChoice
-  } else if (inputType == InputType.Stats) {
-    let xBar, n, equalityChoice
-    // a = x bar
-    // b = n
-    // c = equalityChoice
+// proportions
+
+// one prop z int
+  // 1 sample
+    // random
+    // 10%
+    // large counts
+      // at least 10 successes and failures
+        // np̂ >= 10   and   n(1-p̂) >= 10
+
+function OnePropZInt(x, n, cLevel) {
+  let p̂ = x / n
+  // p hat +- z*sqrt(phat(1-phat)/n)
+  
+}
+
+// one prop z test
+  // 1 sample
+    // random
+    // 10%
+    // large counts
+        // np0 >= 10   and   n(1-p0) >= 10
+
+function OnePropZTest(p0, x, n, equality) {
+
+}
+
+// two prop z int
+  // 2 samples
+    // random
+    // 10% for both samples
+    // large counts
+      // at least 10 successes and failures in both groups
+        // n1p̂1, n1(1-p̂1), n2p̂2, n2(1-p̂2) >= 10
+
+function TwoPropZInt(x1, n1, x2, n2, cLevel) {
+
+}
+
+// two prop z test
+  // 2 samples
+    // random
+    // 10% for both samples
+    // large counts
+      // n1p̂1, n1(1-p̂1), n2p̂2, n2(1-p̂2) >= 10
+
+function TwoPropZTest(x1, n1, x2, n2, equality) {
+
+}
+
+
+
+
+
+
+// means
+
+// t interval
+  // 1 sample
+    // random
+    // 10%
+    // normal / large sample
+      // population distribution normal or sample size large
+        // n >= 30
+      // no strong skewness or outliers if n < 30 and pop has unknown shape
+
+function TInterval(input, a, b, c, d) {
+  if (input = InputType.Data) {
+    // List = a
+    // C-Level = b
+  } else if (input == InputType.Stats) {
+    // x̄ = a
+    // Sx = b
+    // n = c
+    // C-Level = d
   }
 }
 
-function TTest(inputType, μ0, ...rest) {
-  if (inputType == InputType.Data) {
-    let list, equalityChoice
-    // a = list
-    // b = equalityChoice
-  } else if (inputType == InputType.Stats) {
-    let xBar, Sx, n, equalityChoice
-    // a = x bar
-    // b = Sx
-    // c = n
-    // d = equalityChoice
+
+// t test
+  // 1 sample
+    // random
+    // 10%
+    // normal / large sample
+      // population distribution normal or sample size large
+        // n >= 30
+      // no strong skewness or outliers if n < 30 and pop has unknown shape
+
+function TTest(input, μ0, a, b, c, d) {
+  if (input == InputType.Data) {
+    // List = a
+    // equality = b
+  } else if (input == InputType.Stats) {
+    // x̄ = a
+    // Sx = b
+    // n = c
+    // equality = d
   }
 }
 
-function TwoSampZTest(inputType, σ1, σ2, ) {
-  if (inputType == InputType.Data) {
-    let list1, list2, equalityChoice
-    // a = list 1
-    // b = list 2
-    // c = equalityChoice
-  } else if (inputType == InputType.Stats) {
-    let xBar1, n1, xBar2, n2, equalityChoice
-    // a = x bar 1
-    // b = n1
-    // c = x bar 2
-    // d = n2
-    // e = equalityChoice
+// two samp t int
+  // 2 samples
+    // random
+    // 10% for both samples
+    // normal / large samples
+      // population distributions normal or sample sizes large
+        // n1 >= 30 and n2 >= 30
+      // no strong skewness or outliers if n < 30 and pop has unknown shape
+
+function TwoSampTInt(input, a, b, c, d, e, f, g) {
+  if (input == InputType.Data) {
+    // List1 = a
+    // List2 = b
+    // C-Level = c
+  } else if (input == InputType.Stats) {
+    // x̄1 = a
+    // Sx1 = b
+    // n1 = c
+    // x̄2 = d
+    // Sx2 = e
+    // n2 = f
+    // C-Level = g
   }
 }
 
-function TwoSampTTest(inputType, ...rest) {
-  if (inputType == InputType.Data) {
-    let list1, list2, equalityChoice
-    // a = list 1
-    // b = list 2
-    // c = equalityChoice
-  } else if (inputType == InputType.Stats) {
-    let xBar1, Sx1, n1, xBar2, Sx2, n2, equalityChoice
-    // a = x bar 1
-    // b = Sx1
-    // c = n1
-    // d = x bar 2
-    // e = Sx2
-    // f = n2
-    // g = equalityChoice
+// two samp t test
+  // 2 samples
+    // random
+    // 10% for both samples
+    // normal / large samples
+      // population distributions normal or sample sizes large
+        // n1 >= 30 and n2 >= 30
+      // no strong skewness or outliers if n < 30 and pop has unknown shape
+
+function TwoSampTTest(input, a, b, c, d, e, f, g) {
+  if (input == InputType.Data) {
+    // List1 = a
+    // List2 = b
+    // equality = c
+  } else if (input == InputType.Stats) {
+    // x̄1 = a
+    // Sx1 = b
+    // n1 = c
+    // x̄2 = d
+    // Sx2 = e
+    // n2 = f
+    // equality = g
   }
 }
 
-function OnePropZTest(p0, x, n, equalityChoice) {
 
-}
 
-function TwoPropZTest(x1, n1, x2, n2, equalityChoice) {
 
-}
 
-function ZInterval(inputType, σ, ...rest) {
-  if (inputType == InputType.Data) {
-    // a = list 1
-    // b = c level
-  } else if (inputType == InputType.Stats) {
-    // a = x bar
-    // b = n
-    // c = c level
-  }
-}
 
-function TInterval(inputType, ...rest) {
-  if (inputType == InputType.Data) {
-    // a = list 1
-    // b = c level
-  } else if (inputType == InputType.Stats) {
-    // a = x bar
-    // b = Sx
-    // c = n
-    // d = c level
-  }
-}
 
-function TwoSampZInt() {
 
-}
 
-function TwoSampTInt() {
 
-}
 
-function OnePropZInt() {
 
-}
 
-function TwoPropZInt() {
 
+// todo
+
+
+
+function Chi2GOFTest() {
+  
 }
 
 function Chi2Test() {
   
 }
 
-function Chi2GOFTest() {
-  
+function LinRegTInt() {
+
 }
 
 function LinRegTTest() {
 
 }
-
-function LinRegTInt() {
-
-}
-
-
-
-
-
-/*
-
-
-
-
-{
-  "Z-Test": {
-    "items": [
-      {"name": "inpt", "type": "choice", "description": "idk", "options": [
-        {"name": "Data", "description": "idk"},
-        {"name": "Stats", "description": "idk"}
-      ]},
-      {"name": "μ0", "type": "number", "description": "idk"},
-      {"name": "σ", "type": "number", "description": "idk"},
-      {"name": "List", "type": "list", "description": "idk", "option": 0},
-      {"name": "x bar", "type": "number", "description": "idk", "option": 1},
-      {"name": "n", "type": "number", "description": "idk", "option": 1},
-      {"name": "μ", "type": "choice", "description": "idk", "options": [
-        {"name": "≠μ0", "description": "idk"},
-        {"name": "<μ0", "description": "idk"},
-        {"name": ">μ0", "description": "idk"}
-      ]}
-    ],
-    "description": "to write"
-  },
-  "T-Test": {
-    "items": [
-      {"name": "trials", "type": "number", "description": "Number of trials"},
-      {"name": "p", "type": "number", "description": "Probability of success"},
-      {"name": "x value", "type": "number", "description": "idk"}
-    ],
-    "description": "to write"
-  },
-  "2-SampZTest": {
-    "items": [
-      {"name": "trials", "type": "number", "description": "Number of trials"},
-      {"name": "p", "type": "number", "description": "Probability of success"},
-      {"name": "x value", "type": "number", "description": "idk"}
-    ],
-    "description": "to write"
-  },
-  "2-SampTTest": {
-    "items": [
-      {"name": "trials", "type": "number", "description": "Number of trials"},
-      {"name": "p", "type": "number", "description": "Probability of success"},
-      {"name": "x value", "type": "number", "description": "idk"}
-    ],
-    "description": "to write"
-  },
-  "1-PropZTest": {
-
-  },
-  "2-PropZTest": {
-
-  },
-  "ZInterval": {
-
-  },
-  "TInterval": {
-
-  },
-  "2-SampZInt": {
-
-  },
-  "2-SampTInt": {
-
-  },
-  "1-PropZInt": {
-
-  },
-  "2-PropZInt": {
-
-  },
-  "ChiSquared-Test": {
-
-  },
-  "ChiSquaredGOF-Test": {
-
-  },
-  "LinRegTTest": {
-
-  },
-  "LinRegTInt": {
-
-  }
-}
-
-
-*/
