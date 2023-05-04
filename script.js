2faf8484347c894cf8aebadd2ce0c5b97c0f19fd
Alston Rodriguez <alston1399@gmail.com>
2023-05-04 00:13:10 -0700

    demo of precise num

--- script.js
+++ script.js
@@ -5,3 +5,14 @@ grapher.clear()
 grapher.drawAxes()
 
 grapher.graphFunction(myErfApprox.fn, [-5,0], true, false, '#0000ff')
+
+let a = new PreciseNumber(5+rand())
+let b = new PreciseNumber(5+rand())
+
+let c = 5+rand()
+console.log(c, new PreciseNumber(c.toString()))
+
+console.log(a.toString())
+console.log(b.toString())
+
+// console.log(5+rand())
