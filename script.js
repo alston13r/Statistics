986248dab3b4c6b30f133e1947bbde1ed1c5aaa6
Alston Rodriguez <alston1399@gmail.com>
2023-05-03 23:06:10 -0700

    taylor approx of erf demo

--- script.js
+++ script.js
@@ -4,5 +4,4 @@ grapher.appendTo(document.body)
 grapher.clear()
 grapher.drawAxes()
 
-let l1 = new List([134,145,104,119,124,161,107,83,113,129,97,129])
-let l2 = new List([70,118,101,85,107,132,94])
+grapher.graphFunction(myErfApprox.fn, [-5,0], true, false, '#0000ff')
