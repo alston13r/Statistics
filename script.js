3d6a7f339b1e5d21072a828a9836cce25e91b78c
AlstonRodriguez <14238578-AlstonRodriguez@users.noreply.replit.com>
2023-05-09 17:47:37 +0000

    demo of graphing options

--- script.js
+++ script.js
@@ -1,10 +1,7 @@
 let canvas = document.createElement('canvas')
-let grapher = new Graph(canvas, [-10,10], [-10,10], 500)
+let grapher = new Graph(canvas, [-10,10], [-10,10], min(window.innerWidth, window.innerHeight))
 grapher.appendTo(document.body)
 grapher.clear()
 grapher.drawAxes()
 
-grapher.graphFunction(myErfApprox.fn, [-5,0], true, false, '#0000ff')
-
-console.log(PseudoNumber.mul(541.1234, 5427.32))
-console.log(541.1234*5427.32)
+grapher.graphFunction(myErfApprox.fn, [-5,0], '#0000ff', 2, GraphingOptions.Lining)
