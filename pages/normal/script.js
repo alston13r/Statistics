7786d9ce6f096749ff892ccfa3be33c9c6433e18
Alston Rodriguez <alston1399@gmail.com>
2023-05-15 01:27:01 -0700

    uhhh

--- pages/normal/script.js
+++ pages/normal/script.js
@@ -58,19 +58,3 @@ for (let b of document.getElementsByClassName('submitButton')) {
 document.getElementById('submitGraphButton').addEventListener('mousedown', submitGraphDetails)
 document.getElementById('submitPdfButton').addEventListener('mousedown', submitPdfDetails)
 document.getElementById('submitCdfButton').addEventListener('mousedown', submitCdfDetails)
-
-// grapher.graphFunction(myErfApprox.fn, [-5,0], '#0000ff', 2, GraphingOptions.Lining)
-
-// let w = min(window.innerWidth, window.innerHeight)
-
-// let canvas = document.createElement('canvas')
-// let grapher = new GraphingUtility(canvas, w)
-// grapher.appendTo(document.body)
-
-// grapher.background('#000000')
-// grapher.align = TextAlign.Center
-// grapher.baseline = TextBaseline.Middle
-// grapher.font = '40px arial'
-// grapher.noStroke()
-// grapher.fill('#ffffff')
-// grapher.text(res, w/2, w/2)
