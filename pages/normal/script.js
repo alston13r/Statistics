457489c3ed99ae594ec37b87551baa76fba71d56
Alston Rodriguez <alston1399@gmail.com>
2023-05-16 00:57:14 -0700

    super overhual again, but better this time

--- pages/normal/script.js
+++ pages/normal/script.js
@@ -1,72 +1,10 @@
 let graphContainer = document.getElementById('graphContainer')
 let buttonContainer = document.getElementById('buttonContainer')
-let calcOutput = document.getElementById('calcOutput')
 let distInfoContainer = document.getElementById('distInfoContainer')
 let calcInfoContainer = document.getElementById('calcInfoContainer')
 let linkContainer = document.getElementById('linkContainer')
 
-let bw = window.innerWidth
-let bh = window.innerHeight
-
-let marginSpace = 0.01
-let leftWidth = 0.55
-let rightWidth = 1 - leftWidth
-let graphContainerHeight = 0.8
-let buttonContainerHeight = 1 - graphContainerHeight
-let distInfoContainerHeight = 0.45
-let calcInfoContainerHeight = 0.3
-let linkContainerHeight = 1 - distInfoContainerHeight - calcInfoContainerHeight
-
-let t = marginSpace * bw
-let usableW = bw - 3 * t
-let usableLH = bh - 3 * t
-let usableRH = bh - 4 * t
-
-document.querySelector('#left').style.width = leftWidth * 100 + '%'
-document.querySelector('#right').style.width = rightWidth * 100 + '%'
-
-for (let d of document.querySelectorAll('.container')) {
-  d.style.marginTop = bw * marginSpace + 'px'
-}
-for (let d of document.querySelectorAll('#left div')) {
-  d.style.marginLeft = bw * marginSpace + 'px'
-  d.style.width = usableW * leftWidth + 'px'
-}
-for (let d of document.querySelectorAll('#right div')) {
-  d.style.marginLeft = bw * marginSpace / 2 + 'px'
-  d.style.width = usableW * rightWidth + 'px'
-}
-
-let distInfoContainerPadding = 5
-graphContainer.style.height = usableLH * graphContainerHeight + 'px'
-buttonContainer.style.height = usableLH * buttonContainerHeight + 'px'
-distInfoContainer.style.height = usableRH * distInfoContainerHeight - distInfoContainerPadding * 2 + 'px'
-let newDistInfoWidth = distInfoContainer.style.width
-newDistInfoWidth = parseFloat(newDistInfoWidth.substring(0, newDistInfoWidth.length - 2)) - distInfoContainerPadding * 2 + 'px'
-distInfoContainer.style.width = newDistInfoWidth
-calcInfoContainer.style.height = usableRH * calcInfoContainerHeight + 'px'
-linkContainer.style.height = usableRH * linkContainerHeight + 'px'
-
-let buttonContainerMargin = 0.02
-let outputContainerWidth = 0.4
-let buttonWidth = (1 - outputContainerWidth) / 3
-let buttonPadding = 16
-
-let buttcw = buttonContainer.style.width
-let buttch = buttonContainer.style.height
-buttcw = parseFloat(buttcw.substring(0, buttcw.length - 2))
-buttch = parseFloat(buttch.substring(0, buttch.length - 2))
-let usableButtCW = buttcw * (1 - 5 * buttonContainerMargin)
-let usableButtCH = buttch - buttcw * buttonContainerMargin
-
-for (let b of document.querySelectorAll('.coolButton')) {
-  b.style.marginTop = buttcw * buttonContainerMargin + 'px'
-  b.style.width = usableButtCW * buttonWidth - buttonPadding + 'px'
-  b.style.height = usableButtCH - buttonPadding * 2 + 'px'
-}
-calcOutput.style.marginTop = buttcw * buttonContainerMargin + 'px'
-calcOutput.style.width = usableButtCW * outputContainerWidth - buttonPadding + 'px'
-calcOutput.style.height = usableButtCH - buttonPadding * 2 + 'px'
+let calcOutput = document.getElementById('calcOutput')
 
 let canvas = document.createElement('canvas')
 let gw = graphContainer.clientWidth
@@ -76,8 +14,6 @@ grapher.appendTo(graphContainer)
 grapher.clear()
 grapher.drawAxes()
 grapher.graphNormalCurve(0, 1, '#00f', 2)
-
-let calcOuputContainer = document.getElementById('calcOutput')
 
 let meanInput = document.getElementById('meanInput')
 let stdevInput = document.getElementById('stdevInput')
@@ -99,7 +35,7 @@ function submitPdfDetails() {
   let x = parseInt(xInput.value)
   let mean = parseFloat(meanInput.value)
   let stdev = parseFloat(stdevInput.value)
-  calcOuputContainer.innerHTML = 'Output:<br>' + NormalPDF(x, mean, stdev)
+  calcOutput.innerHTML = 'Output:<br>' + NormalPDF(x, mean, stdev)
 }
 
 let lowerInput = document.getElementById('lowerInput')
@@ -111,7 +47,7 @@ function submitCdfDetails() {
   let upper = parseFloat(upperInput.value)
   let mean = parseFloat(meanInput.value)
   let stdev = parseFloat(stdevInput.value)
-  calcOuputContainer.innerHTML = 'Output:<br>' + NormalCDF(lower, upper, mean, stdev)
+  calcOutput.innerHTML = 'Output:<br>' + NormalCDF(lower, upper, mean, stdev)
 }
 
 for (let b of document.getElementsByClassName('coolButton')) {
@@ -120,6 +56,10 @@ for (let b of document.getElementsByClassName('coolButton')) {
     b.style.backgroundColor = '#d00'
   })
   b.addEventListener('mouseup', () => {
+    b.style.border = 'outset #a00 3px'
+    b.style.backgroundColor = '#f00'
+  })
+  b.addEventListener('mouseout', () => {
     b.style.border = 'outset #a00 3px'
     b.style.backgroundColor = '#f00'
   })
@@ -132,3 +72,8 @@ let cdfButton = document.getElementById('calcCDF')
 graphItButton.addEventListener('mousedown', submitGraphDetails)
 pdfButton.addEventListener('mousedown', submitPdfDetails)
 cdfButton.addEventListener('mousedown', submitCdfDetails)
+
+window.onresize = () => {
+  grapher.grapher.size = [graphContainer.clientWidth, graphContainer.clientHeight]
+  submitGraphDetails()
+}
