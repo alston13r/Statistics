abd91a8c451e712551c9d3f5001ad4965b5a55e4
alston13r <63579820+alston13r@users.noreply.github.com>
2023-05-20 06:08:47 +0000

    hmmmm

--- index.js
+++ index.js
@@ -20,35 +20,36 @@ http.createServer((req, res) => {
     console.log('Got pinged so I ponged!')
     return
   }
+  
   url = url.slice(url.length-2)
-  let type = 'text/plain'
+  let type = typeEquiv.html
   let location = htmlDirectory+'index.html'
-  switch (url[0]) {
-    case '':
-      if (url[1] == 'favicon') {
-        type = typeEquiv.favicon
-        location = faviconDirectory+'favicon.ico'
-      } else {
+  if (url[1] == 'favicon') {
+    type = typeEquiv.favicon
+    location = faviconDirectory+'favicon.ico'
+  } else {
+    switch (url[0]) {
+      case '':
         type = typeEquiv.page
         location = htmlDirectory+'index.html'
-      }
-      break
-    case 'styles':
-      type = typeEquiv.style
-      location = styleDirectory+url[1]+'.css'
-      break
-    case 'distributions':
-      type = typeEquiv.page
-      location = htmlDirectory+url[1]+'.html'
-      break
-    case 'scripts':
-      type = typeEquiv.script
-      location = scriptDirectory+url[1]+'.js'
-      break
-    default:
-      type = 'text/plain'
-      location = htmlDirectory+'index.html'
-      break
+        break
+      case 'styles':
+        type = typeEquiv.style
+        location = styleDirectory+url[1]+'.css'
+        break
+      case 'distributions':
+        type = typeEquiv.page
+        location = htmlDirectory+url[1]+'.html'
+        break
+      case 'scripts':
+        type = typeEquiv.script
+        location = scriptDirectory+url[1]+'.js'
+        break
+      default:
+        type = typeEquiv.page
+        location = htmlDirectory+'index.html'
+        break
+    }
   }
   res.writeHead(200, {'Content-Type': type})
   fs.createReadStream(location).pipe(res)
